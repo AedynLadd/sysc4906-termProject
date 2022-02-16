8952395e4ddc85613ae482cb85654a0142c54776
Aedyn Ladd <55495779+AedynLadd@users.noreply.github.com>
2022-02-16 15:14:26 -0500

    text processing for reddit
    some processing and reformatting of raw reddit data - is able to be modified to work with any kind of text data as required

--- src/data/text_process.py
+++ src/data/text_process.py
@@ -1,12 +1,18 @@
 
+from concurrent.futures import process
+from lib2to3.pgen2 import token
 import os
+import re
 import json
 import logging
+from numpy import true_divide
 import pandas as pd
 from pathlib import Path
 
 logger = logging.getLogger("TextProcessing")
 project_dir = "./"
+
+text_process_config = json.load(open("{}/src/data/config/text_process_config.json".format(project_dir)))
 
 def files_to_analyze(project_path):
     """
@@ -14,13 +20,55 @@ def files_to_analyze(project_path):
     """
     return [filename for filename in Path(project_path).glob('*')]
 
-def load_raw_data():
-    return None
+def tokenize(input_text):
+    """
+        We begin with tokenization - we split our text into just words
+        all words will be lowercase and punctuation will be removed
+    """
+    reg_expressions = [r'http\S+',  # Remove any hyperlinks
+                       r'[\W]+',    # Identify words
+                       r'[0-9]+'    # Identify and remove numbers
+                      ]
+    for reg in reg_expressions:
+        input_text = re.sub(reg, " ", input_text)
 
-def process_text():
+    return input_text.lower()
 
-    return 200
+def filter_raw(loaded_raw):
+    """
+        defines all necessary steps to filter our raw data
+    """
+    tokenized_text = ""
 
+    try:
+        tokenized_text = [token for token in tokenize(loaded_raw).split(" ") if len(token) > 1]
+        tokenized_text = " ".join(tokenized_text)
+    except Exception as e:
+        logger.error("An Error Occured Tokenizing : {}".format(e))
+    finally:
+        return tokenized_text
+
+def process_data(filename):
+    """
+        Process the data
+    """
+    processed_data = pd.DataFrame()
+
+    try:
+        # Our first step is to load in the data from our raw json file:
+        loaded_raw = pd.read_json(filename, orient="index")
+        # Our next step is to filter our raw data - This will include both the selftext and the title
+        loaded_raw["combined_text_data"] = loaded_raw["title"] + loaded_raw["selftext"]
+        # Apply our text filter to this new column
+        processed_data["timestamp"] = loaded_raw["created_utc"]
+        processed_data["data"] = loaded_raw["combined_text_data"].apply(filter_raw)
+        processed_data["score"] = loaded_raw["score"]
+
+    except Exception as e:
+        logger.info("An Error Occured when processing at the filepath {}: {}".format(filename, e))
+
+    finally:
+        return processed_data
 
 if __name__ == '__main__':
     try:
@@ -34,15 +82,17 @@ if __name__ == '__main__':
 
         logger.info("Running in {}".format(project_dir))
 
-        text_process_config = json.load(open("{}/src/data/config/text_process_config.json".format(project_dir)))
-
         for data_source_name in text_process_config["data_stores"]:
             try:
-                data_load = files_to_analyze("{}/data/raw/{}".format(project_dir, data_source_name))
+                data_source_path = "{}/data/raw/{}".format(project_dir, data_source_name)
+                
+                analyzed_data = pd.concat([process_data(filename) for filename in Path(data_source_path).glob('*') if (not ("gitkeep" in str(filename)))])
+                analyzed_data = analyzed_data.sort_values(by="timestamp", ascending=True, ignore_index=True)
+                # Save data to a json
+                analyzed_data.to_json("{}/data/interim/{}_processed.json".format(project_dir, data_source_name), orient="records")
 
-                logger.info(data_load)
             except Exception as e:
-                logger.info("Error Occured when processing {} data".format(data_source_name))
+                logger.error("Error Occured when processing {} data: {}".format(data_source_name, e))
         
         logger.info("Text Analysis Completed")
     except Exception as e:
