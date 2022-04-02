f93fc56a9c873eb094ebdff88f83f2cd971e8d16
Yunas Magsi <yunasmagsi@cmail.carleton.ca>
2022-04-02 18:00:16 -0400

    Updated button, and adding some help function vocabulary description
    working on the question mark button

--- dashboard/scripts/help_menu/visual_info.js
+++ dashboard/scripts/help_menu/visual_info.js
@@ -1,30 +1,30 @@
 const help_menu_visual_info = {
     "coin_select":{
         "name": "",
-        "description": ""
+        "description": "The list of top 100 can be seen here"
     },
     "cyclic_coin_info":{
         "name": "",
-        "description": ""
+        "description": "Shows a list of 5 coins that are constantly refreshing, if you see you're desired coin here, click it to view more information!"
     }, 
     "keyword_network_diagram":{
         "name": "",
-        "description": ""
+        "description": "The keyword network Diagram, will show inter-relationships of between different crypto currencies"
     },
     "network_diagram_data":{
         "name": "",
-        "description": ""
+        "description": "When selected on a specfic Coin the Data of the specific coin will show ______________#tbd"
     },
     "coin_chart_data":{
         "name": "",
-        "description": ""
+        "description": "The Coin Chart Data will show a 1 year past time line of the specified coin price opening price day to day"
     },
     "coin_sentiment_heatmap":{
         "name": "",
-        "description": ""
+        "description": "The Heat map will show a concentration of how many hits theres where on a speific day on a certain subreddits"
     },
     "forecast_coin_chart":{
         "name": "",
-        "description": ""
+        "description": "When Specificed on a certain coin the coin chart data will show the opening price of a crypto coin, along with the future prediction of the estimated value range of coin for the future shown in white"
     }
 }
