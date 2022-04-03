3b9edd3d14d945d81a0a63f711361fab988e6afd
Yunas Magsi <yunasmagsi@cmail.carleton.ca>
2022-04-02 20:09:16 -0400

    working on hover feature on help menu, where when a user  presses question, it display info on console temperary

--- dashboard/scripts/help_menu/help_menu.js
+++ dashboard/scripts/help_menu/help_menu.js
@@ -1,6 +1,7 @@
 
 help_modal = document.getElementById("settings_modal");
 
+
 function create_help_card(){
     return [
         "<div>",
@@ -9,11 +10,47 @@ function create_help_card(){
 }
 
 
+
 function openHelpMenu(){
     console.log("Opening Menu")
-
+    console.log(help_menu_visual_info)
+    displayMouseHoeverInformation()
+    
 }
 
 function closeHelpMenu(){
     console.log("Closing Menu")
+}
+function displayMouseHoeverInformation(){
+    document.getElementById("coin_select").onmouseover = function() {mouseOver()};
+    document.getElementById("cyclic_coin_info").onmouseover = function() {mouseOver()};
+    document.getElementById("keyword_network_diagram").onmouseover = function() {mouseOver()};
+    document.getElementById("network_diagram_data").onmouseover = function() {mouseOver()};
+    document.getElementById("coin_chart_data").onmouseover = function() {mouseOver()};
+    document.getElementById("coin_sentiment_heatmap").onmouseover = function() {mouseOver()};
+    document.getElementById("forecast_coin_chart").onmouseover = function() {mouseOver()};
+}
+
+function mouseOver(){
+    if (document.getElementById("coin_select")) {
+        console.log(help_menu_visual_info.coin_select.name + help_menu_visual_info.coin_select.description);
+    }
+    if (document.getElementById("cyclic_coin_info")) {
+        console.log(help_menu_visual_info.cyclic_coin_info.name + help_menu_visual_info.cyclic_coin_info.description);
+    }
+    if (document.getElementById("keyword_network_diagram")) {
+        console.log(help_menu_visual_info.keyword_network_diagram.name + help_menu_visual_info.keyword_network_diagram.description);
+    }
+    if (document.getElementById("network_diagram_data")) {
+        console.log(help_menu_visual_info.network_diagram_data.name + help_menu_visual_info.network_diagram_data.description);
+    }
+    if (document.getElementById("coin_chart_data")) {
+        console.log(help_menu_visual_info.coin_chart_data.name + help_menu_visual_info.coin_chart_data.description);
+    }
+    if (document.getElementById("coin_sentiment_heatmap")) {
+        console.log(help_menu_visual_info.coin_sentiment_heatmap.name + help_menu_visual_info.coin_sentiment_heatmap.description);
+    }
+    if (document.getElementById("forecast_coin_chart")) {
+        console.log(help_menu_visual_info.forecast_coin_chart.name + help_menu_visual_info.forecast_coin_chart.description);
+    }
 }
--- dashboard/scripts/help_menu/visual_info.js
+++ dashboard/scripts/help_menu/visual_info.js
@@ -1,30 +1,30 @@
 const help_menu_visual_info = {
     "coin_select":{
-        "name": "",
+        "name": "Coin Selection",
         "description": "The list of top 100 can be seen here"
     },
     "cyclic_coin_info":{
-        "name": "",
-        "description": "Shows a list of 5 coins that are constantly refreshing, if you see you're desired coin here, click it to view more information!"
+        "name": "Cycling Coin",
+        "description": "The Cyclic coin information shows a list of 5 coins that are constantly refreshing, if you see you're desired coin here, click it to view more information!"
     }, 
     "keyword_network_diagram":{
-        "name": "",
+        "name": "Keyword Network Diagram",
         "description": "The keyword network Diagram, will show inter-relationships of between different crypto currencies"
     },
     "network_diagram_data":{
-        "name": "",
+        "name": "Keyword Network Diagram Info",
         "description": "When selected on a specfic Coin the Data of the specific coin will show ______________#tbd"
     },
     "coin_chart_data":{
-        "name": "",
-        "description": "The Coin Chart Data will show a 1 year past time line of the specified coin price opening price day to day"
+        "name": "Coin Chart Data For Year",
+        "description": "The Coin Chart Data will show a 1 year past time line of the specified coin price opening day price on a line chart"
     },
     "coin_sentiment_heatmap":{
-        "name": "",
-        "description": "The Heat map will show a concentration of how many hits theres where on a speific day on a certain subreddits"
+        "name": "Coin Sentiment Heatmap From Subreddits",
+        "description": "The Heat map will show a concentration of how many hits theres where of the selected coin on a speific day on a certain subreddits"
     },
     "forecast_coin_chart":{
-        "name": "",
+        "name": "Predicted Coin Price",
         "description": "When Specificed on a certain coin the coin chart data will show the opening price of a crypto coin, along with the future prediction of the estimated value range of coin for the future shown in white"
     }
 }
